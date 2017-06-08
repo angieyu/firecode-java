38b5744611cbda1a6520cd46ca18e2d2314858d5
angieyu <yu.angela.c@gmail.com>
2017-06-07 18:15:56 -0700

    m

---
+++ src/bubbleSortArray.java
@@ -0,0 +1,32 @@
+import static java.lang.System.out;
+
+/**
+ * Created by angie on 6/7/17.
+ */
+public class bubbleSortArray {
+    public static int[] bubbleSortArray(int[] arr) {
+        // two nested loops
+        // 1st is a reverse loops
+        // use a tail index to keep track of elements moved to the end
+        // outer loop decrements the tail index
+        // nested loop iterate from the start to the tail
+        // use temp var to do the swap
+        int tail = arr.length - 1;
+        for (int i = tail; i >= 0; i--) {
+            for (int j = 0; j < tail; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            tail--;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] testArr = {5, 4, 3};
+        out.println(bubbleSortArray(testArr));
+    }
+}
---
+++ src/replace.java
@@ -0,0 +1,24 @@
+import static java.lang.System.out;
+
+/**
+ * Created by angie on 6/7/17.
+ */
+public class replace {
+    public static String replace(String a, String b) {
+        StringBuilder A = new StringBuilder(a);
+        StringBuilder B = new StringBuilder(b);
+        for (int i = 0; i < A.length(); i++) {
+            int space = A.indexOf(" ");
+            if (space > 0) {
+                out.println(space);
+                A.delete(space, space + 1);
+                A.insert(space, B);
+            }
+        }
+        return A.toString();
+    }
+
+    public static void main(String[] args) {
+        out.println(replace("hi this is angela", "ABC"));
+    }
+}
